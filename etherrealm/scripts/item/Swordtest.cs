ef619c0dac368cc9a605f546a7b2a01df5a64919
LukoRaptor <lukas.cikan@gmail.com>
2025-09-04 17:12:05 +0200

    Weapon class rework + adjustments
    Modified Weapon class so it is correct and adjusted Swordtest to fit it. Added swing animation.

--- etherrealm/scripts/item/Swordtest.cs
+++ etherrealm/scripts/item/Swordtest.cs
@@ -4,74 +4,26 @@ using System;
 public partial class Swordtest : Weapon
 {
     [Export] private Sprite2D sprite { get; set; }
-    [Export] private Area2D hitbox { get; set; }
-    
-    private Tween stabTween;
-    private CollisionShape2D stabCollider;
-    private bool isAttacking = false;
+    [Export] private Area2D _hitbox { get; set; }
     
     //this weapons properties
-    private float stabDistance = 10f;
-    private float _stabDamage = 10;
+    private float _attackDistance = 10f;
+    private float _attackDamage = 10;
     private float _crit = 15; //percentage chance to crit
     private float _knockback = 10;
 
     public override void _Ready()
     {
-        damage = _stabDamage;
+        damage = _attackDamage;
         critChance = _crit;
-        stabCollider = GetNode<CollisionShape2D>("hitbox/collider");
         knockback = _knockback;
+        hitbox = _hitbox;
+        attackCollider = GetNode<CollisionShape2D>("hitbox/collider");
+        stabDistance = _attackDistance;
     }
     
-    public override void _Input(InputEvent @event)
+    public override void Attack()
     {
-        if (@event is InputEventMouseButton mouseEvent)
-        {
-            if (mouseEvent.IsActionPressed("MouseLeftButton"))
-            {
-                //rotate to mouse
-                LookAt(GetGlobalMousePosition());
-                Stab();
-            }
-        }
-    }
-
-    public override void _Process(double delta)
-    {
-        if (isAttacking)
-        {
-            Visible = true;
-            stabCollider.Disabled = false;
-        }
-        else
-        {
-            Visible = false;
-            stabCollider.Disabled = true;
-        }
-    }
-    
-    public override void Stab()
-    {
-        //let the stab finish so 1) no spamming 2) the sword doesn't fly off
-        if (stabTween != null)
-            return;
-        
-        var origin = Position;
-        var mouseDir = (GetGlobalMousePosition() - GlobalPosition).Normalized();
-        hitDir = mouseDir;
-
-        stabTween = GetTree().CreateTween().BindNode(this);
-        stabTween.TweenProperty(this, "position", origin + (mouseDir * stabDistance), 0.1f);
-        stabTween.TweenProperty(this, "position", origin, 0.1f).SetDelay(0.1f);
-        
-        isAttacking = true;
-        
-        //make it null after finishing
-        stabTween.Finished += () =>
-        {
-            isAttacking = false;
-            stabTween = null;
-        };
+        Stab();
     }
 }
--- etherrealm/scripts/parent classes/Weapon.cs
+++ etherrealm/scripts/parent classes/Weapon.cs
@@ -7,22 +7,104 @@ public partial class Weapon : Node2D
 	public float critChance; // chance in percent to do critical hit
 	public float knockback;
 	public Vector2 hitDir;
+	public Area2D hitbox { get; set; }
+	public float stabDistance;
+	public Tween attackTween;
+	public CollisionShape2D attackCollider;
+	public bool isAttacking = false;
 	
-	public virtual void Input(InputEvent @event)
+	public override void _Input(InputEvent @event)
 	{
-		// zjistí, že bylo stisknuto tlačítko myši
 		if (@event is InputEventMouseButton mouseEvent)
 		{
 			if (mouseEvent.IsActionPressed("MouseLeftButton"))
 			{
-				// otočí Node2D tak, aby směřoval na pozici myši (globální souřadnice)
+				//rotate to mouse
 				LookAt(GetGlobalMousePosition());
+				Attack();
 			}
 		}
 	}
-
-	public virtual void Stab()
+	
+	public override void _Process(double delta)
 	{
+		if (isAttacking)
+		{
+			Visible = true;
+			attackCollider.Disabled = false;
+		}
+		else
+		{
+			Visible = false;
+			attackCollider.Disabled = true;
+		}
+	}
 
+	public virtual void Attack()
+	{
+		Stab();
+	}
+	
+	public void Stab()
+	{
+		//let the stab finish so 1) no spamming 2) the sword doesn't fly off
+		if (attackTween != null)
+			return;
+        
+		var origin = Position;
+		var mouseDir = (GetGlobalMousePosition() - GlobalPosition).Normalized();
+		hitDir = mouseDir;
+
+		attackTween = GetTree().CreateTween().BindNode(this);
+		attackTween.TweenProperty(this, "position", origin + (mouseDir * stabDistance), 0.1f);
+		attackTween.TweenProperty(this, "position", origin, 0.1f).SetDelay(0.1f);
+        
+		isAttacking = true;
+        
+		//make it null after finishing
+		attackTween.Finished += () =>
+		{
+			isAttacking = false;
+			attackTween = null;
+		};
+	}
+
+	public void Swing()
+	{
+		if (attackTween != null)
+			return;
+
+		Vector2 mouseDir = (GetGlobalMousePosition() - GlobalPosition).Normalized();
+		float targetAngle = mouseDir.Angle();
+
+		float swingAngle = Mathf.Pi / 2f; // 90 degrees
+
+		// Determine if swing is to the left or right
+		bool rightSide = mouseDir.X > 0;
+		float startAngle, endAngle;
+
+		if (rightSide)
+		{
+			// Clockwise swing
+			startAngle = targetAngle - swingAngle / 2f;
+			endAngle = targetAngle + swingAngle / 2f;
+		}
+		else
+		{
+			// Counterclockwise swing
+			startAngle = targetAngle + swingAngle / 2f;
+			endAngle = targetAngle - swingAngle / 2f;
+		}
+
+		Rotation = startAngle;
+		attackTween = GetTree().CreateTween().BindNode(this);
+		attackTween.TweenProperty(this, "rotation", endAngle, 0.2f);
+
+		isAttacking = true;
+		attackTween.Finished += () =>
+		{
+			isAttacking = false;
+			attackTween = null;
+		};
 	}
 }
